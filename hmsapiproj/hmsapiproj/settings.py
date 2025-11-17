65c03cd01c06a0fe0af369a34a18338817a52e38
ashna-shibu <ashnashibu18@gmail.com>
2025-11-17 17:27:11 +0530

    Admin Changes

--- hmsapiproj/hmsapiproj/settings.py
+++ hmsapiproj/hmsapiproj/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    # 'apibackendapp',
+    'apibackendapp',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'admins'
